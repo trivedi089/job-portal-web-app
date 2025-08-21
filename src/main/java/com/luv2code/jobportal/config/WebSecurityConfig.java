47cb50903e2bdc1c9608842d6c2bf36085949921
trivedi089 <satyachbs007@gmail.com>
2025-08-21 17:00:03 +0530

    custom-authentication-success-handler

--- src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     private final String[] publicUrl = {"/",
             "/global-search/**",
@@ -30,8 +32,9 @@ public class WebSecurityConfig {
             "/*.js.map",
             "/fonts**", "/favicon.ico", "/resources/**", "/error"};
 
-    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
         this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
     }
 
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -42,6 +45,14 @@ public class WebSecurityConfig {
                 auth.requestMatchers(publicUrl).permitAll();
                 auth.anyRequest().authenticated();
     });
+        http.formLogin(form->form.loginPage("/login")
+                .permitAll().successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl(("/logout"));
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf->csrf.disable());
+
         return http.build();
     }
 
