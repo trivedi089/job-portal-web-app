3797752c07a02376676830bb6efb62173847c4f2
trivedi089 <satyachbs007@gmail.com>
2025-09-01 00:06:45 +0530

    recruiter-login

---
+++ src/main/java/com/luv2code/jobportal/entity/RecruiterProfileService.java
@@ -0,0 +1,22 @@
+package com.luv2code.jobportal.entity;
+
+import com.luv2code.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id){
+        return recruiterProfileRepository.findById(id);
+    }
+}
